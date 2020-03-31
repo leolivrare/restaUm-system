d8403461b216b4d14158883e88f1137f2dd01ed9
leolivrare <l220120@dac.unicamp.br>
2020-03-31 15:29:01 -0300

    Ultimas alteracoes no sistema de print

--- src/application/Application.java
+++ src/application/Application.java
@@ -15,14 +15,19 @@ public class Application {
 		String commands[] = csv.requestCommands();
 		String comando[] = commands[0].split(":");
 		
+		System.out.println("Tabuleiro inicial:");
+		
 		
 		
 		UI.printBoard(match.getPieces(), match.getBoard());
+		System.out.println();
 		
 		match.performMove(new OneLeftPosition(comando[0].charAt(0), Character.getNumericValue(comando[0].charAt(1))), new OneLeftPosition(comando[1].charAt(0), Character.getNumericValue(comando[1].charAt(1))));
 		UI.printBoard(match.getPieces(), match.getBoard());
+		System.out.println();
 		comando = commands[1].split(":");
 		match.performMove(new OneLeftPosition(comando[0].charAt(0), Character.getNumericValue(comando[0].charAt(1))), new OneLeftPosition(comando[1].charAt(0), Character.getNumericValue(comando[1].charAt(1))));
 		UI.printBoard(match.getPieces(), match.getBoard());
+		System.out.println();
 	}
 }
