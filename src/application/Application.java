f6c6d13062230a00643126d20a6b00bb44a023da
Mateus Gabi Moreira <mateusgabimoreira@gmail.com>
2020-04-02 14:54:49 -0300

    removido código anterior

--- src/application/Application.java
+++ src/application/Application.java
@@ -32,35 +32,5 @@ public class Application {
 			UI.printBoard(match.getPieces(), match.getBoard());
 			System.out.println();
 		}
-		
-		// match.performMove(new OneLeftPosition('f', 4), new OneLeftPosition('d', 4));
-		// UI.printBoard(match.getPieces(), match.getBoard());
-		
-		// match.performMove(new OneLeftPosition('c', 4), new OneLeftPosition('e', 4));
-		// UI.printBoard(match.getPieces(), match.getBoard());
-
-		// match.performMove(new OneLeftPosition('c', 2), new OneLeftPosition('c', 4));
-		// UI.printBoard(match.getPieces(), match.getBoard());		
-
-		// match.performMove(new OneLeftPosition('d', 6), new OneLeftPosition('d', 4));
-		// UI.printBoard(match.getPieces(), match.getBoard());	
-
-		// match.performMove(new OneLeftPosition('f', 5), new OneLeftPosition('d', 5));
-		// UI.printBoard(match.getPieces(), match.getBoard());		
-
-		// match.performMove(new OneLeftPosition('c', 5), new OneLeftPosition('e', 5));
-		// UI.printBoard(match.getPieces(), match.getBoard());		
-
-		// match.performMove(new OneLeftPosition('d', 3), new OneLeftPosition('d', 5));
-		// UI.printBoard(match.getPieces(), match.getBoard());
-
-		// match.performMove(new OneLeftPosition('d', 5), new OneLeftPosition('f', 5));
-		// UI.printBoard(match.getPieces(), match.getBoard());	
-
-		// match.performMove(new OneLeftPosition('e', 3), new OneLeftPosition('e', 5));
-		// UI.printBoard(match.getPieces(), match.getBoard());	
-
-		// match.performMove(new OneLeftPosition('b', 4), new OneLeftPosition('d', 4));
-		// UI.printBoard(match.getPieces(), match.getBoard());		
 	}
 }
